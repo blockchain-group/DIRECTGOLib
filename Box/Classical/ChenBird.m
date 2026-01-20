f81ffe09934e932344bfe49f6230d43b0676be52
blockchain-group <remigijus.paulavicius@mif.vu.lt>
2026-01-20 11:48:34 +0200

    git commit -m "Fix Chen's functions (closes #2)"

--- Box/Classical/ChenBird.m
+++ Box/Classical/ChenBird.m
@@ -4,15 +4,15 @@ function y = ChenBird(x)
 % Name:
 %   ChenBird.m
 %
-% References:				
+% References:					
 %  - Momin Jamil and Xin-She Yang, A literature survey of benchmark functions for
 %    global optimization problems, Int. Journal of Mathematical Modelling and
 %    Numerical Optimisation, Vol. 4, No. 2, pp. 150–194 (2013).
 %    DOI: 10.1504/IJMMNO.2013.055204
 %
 % Globally optimal solution:
-%   f = -2000.00000000000022737368
-%   x = [0.38888888888720885006; 0.72222222222796972346]
+%   f = -2000.003999984000064
+%   x = [0.5; 0.5]
 %
 % Default variable bounds:
 %   -500 <= x(i) <= 500, i = 1,...,n
@@ -24,7 +24,7 @@ function y = ChenBird(x)
 %
 % Known characteristics of test function:
 %   Differentiable, Non-separable, Non-scalable, Multi-modal,
-%   Non-convex, Non-plateau, Non-Zero-Solution, Asymmetric
+%   Non-convex, Non-plateau, Non-Zero-Solution, Symmetric
 % -------------------------------------------------------------------------
 if nargin == 0
     y.nx = 2;
@@ -34,14 +34,15 @@ if nargin == 0
     y.xu = @(nx) get_xu(nx);
     y.fmin = @(nx) get_fmin(nx);
     y.xmin = @(nx) get_xmin(nx);
-    y.features = [1, 0, 0, 1, 0, 0, 0. 0];
+    y.features = [1, 0, 0, 1, 0, 0, 0, 1];
     y.libraries = [0, 0, 0, 0, 1, 0, 0, 0, 0, 0];
     return
 end
 if size(x, 2) > size(x, 1), x = x'; end
 
-y = -(0.001/(0.001^2 + (x(1) - 0.4* x(2) - 0.1)^2)) -...
-    (0.001/(0.001^2 + (2*x(1) + x(2) - 1.5)^2));
+b = 1e-3;
+y = -(b/(b^2 + (x(1)^2 + x(2)^2 - 1)^2)) - ...
+     (b/(b^2 + (x(1)^2 + x(2)^2 - 0.5)^2)) - (b/(b^2 + (x(1) - x(2))^2));
 end  
 
 function xl = get_xl(nx)
@@ -53,9 +54,9 @@ function xu = get_xu(nx)
 end
 
 function fmin = get_fmin(~)
-    fmin = -2000.00000000000022737368;
+    fmin = -2000.003999984000064;
 end
 
 function xmin = get_xmin(~)
-    xmin = [0.38888888888720885006; 0.72222222222796972346];
+    xmin = [0.5; -0.5];
 end
